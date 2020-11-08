26b63f4fa177369a00a8031b6fdebbe6cf07ed9e
Dipak Gupta <dipakg286@gmail.com>
2020-11-08 22:23:42 +0530

    netflix clone

---
+++ src/requests.js
@@ -0,0 +1,13 @@
+const API_KEY = "60ee5870176d87ad4ab585cf49018c7a";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_geners=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_geners=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_geners=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_geners=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_geners=99`,
+};
+export default requests;
